b8764663f51b91336a72638792f29c48999be681
Sanskar <sanskarin@outlook.in>
2026-08-09 08:17:51 +0530

    feat(database): advance Finora schema version to 2

--- src/Finora.Shared/AppConstants.cs
+++ src/Finora.Shared/AppConstants.cs
@@ -1,1 +1,14 @@
-namespace Finora.Shared;public static class AppConstants{public const string ProductName="Finora";public const string DatabaseFileName="finora.db3";public const int DatabaseSchemaVersion=1;public const string BackupMagic="FINORA01";public const string RepositoryUrl="https://github.com/sanskarIN/Finora";public const string CreatorProfileUrl="https://www.github.com/sanskarIN";public const string BusinessEmail="sanskarin@outlook.in";public const string SupportEmail="supportramsandesh@gmail.com";public const string Watermark="Made by the Sanskar";}
+namespace Finora.Shared;
+
+public static class AppConstants
+{
+    public const string ProductName = "Finora";
+    public const string DatabaseFileName = "finora.db3";
+    public const int DatabaseSchemaVersion = 2;
+    public const string BackupMagic = "FINORA01";
+    public const string RepositoryUrl = "https://github.com/sanskarIN/Finora";
+    public const string CreatorProfileUrl = "https://www.github.com/sanskarIN";
+    public const string BusinessEmail = "sanskarin@outlook.in";
+    public const string SupportEmail = "supportramsandesh@gmail.com";
+    public const string Watermark = "Made by the Sanskar";
+}
