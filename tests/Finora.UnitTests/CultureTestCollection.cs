700a606b756bf10cab33323dd2c7b0333489998f
Sanskar <sanskarin@outlook.in>
2026-08-09 11:21:47 +0530

    test(localization): serialize tests that mutate process culture

---
+++ tests/Finora.UnitTests/CultureTestCollection.cs
@@ -0,0 +1,7 @@
+namespace Finora.UnitTests;
+
+[CollectionDefinition(Name, DisableParallelization = true)]
+public sealed class CultureTestCollection
+{
+    public const string Name = "Culture-sensitive tests";
+}
