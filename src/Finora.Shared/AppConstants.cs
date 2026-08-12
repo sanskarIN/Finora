a8a162685b1ad769a9b56a42eb630d4318c3636e
Sanskar <sanskarin@outlook.in>
2026-08-12 15:02:38 +0530

    feat(identity): add Buy Me a Coffee project support link

--- src/Finora.Shared/AppConstants.cs
+++ src/Finora.Shared/AppConstants.cs
@@ -8,6 +8,7 @@ public static class AppConstants
     public const string BackupMagic = "FINORA01";
     public const string RepositoryUrl = "https://github.com/sanskarIN/Finora";
     public const string CreatorProfileUrl = "https://www.github.com/sanskarIN";
+    public const string BuyMeACoffeeUrl = "https://buymeacoffee.com/sanskarIN";
     public const string BusinessEmail = "sanskarin@outlook.in";
     public const string SupportEmail = "supportramsandesh@gmail.com";
     public const string Watermark = "Made by the Sanskar";
