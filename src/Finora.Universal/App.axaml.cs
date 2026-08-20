06202e968672e4d5f94abf559a16a60dfe387694
Sanskar <sanskarin@outlook.in>
2026-08-20 06:21:24 +0530

    feat(universal): bootstrap desktop and single-view lifetimes

---
+++ src/Finora.Universal/App.axaml.cs
@@ -0,0 +1,30 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using Finora.Universal.ViewModels;
+using Finora.Universal.Views;
+
+namespace Finora.Universal;
+
+public partial class App : Application
+{
+    public static IUniversalRuntime Runtime { get; set; } = new UnconfiguredUniversalRuntime();
+
+    public override void Initialize() => AvaloniaXamlLoader.Load(this);
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        var viewModel = new MainViewModel(Runtime);
+
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            desktop.MainWindow = new MainWindow { DataContext = viewModel };
+        }
+        else if (ApplicationLifetime is ISingleViewApplicationLifetime singleView)
+        {
+            singleView.MainView = new MainView { DataContext = viewModel };
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+}
